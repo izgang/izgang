1067eda334ad0962aebf4ba7d7671d9da6b5e786
Siong-Ui Te <siongui@gmail.com>
2016-02-16 21:48:53 +0800

    parse title category content

--- tool/fetch2.py
+++ tool/fetch2.py
@@ -11,6 +11,8 @@ def parsePost(path):
         soup = BeautifulSoup(f)
         article = soup.find(id="article-box")
         #print(article.prettify())
+
+        # parse datetime
         # http://stackoverflow.com/questions/5041008/handling-class-attribute-in-beautifulsoup
         month = article.find("span", class_="month").string.strip()
         day = article.find("span", class_="date").string.strip()
@@ -19,9 +21,22 @@ def parsePost(path):
         # http://stackoverflow.com/questions/466345/converting-string-into-datetime
         # https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations
         tstr = month + " " + day + " " + year + " " + hm
-        print(tstr)
+        #print(tstr)
+        # http://stackoverflow.com/questions/13182075/how-to-convert-a-timezone-aware-string-to-datetime-in-python-without-dateutil
         dt = datetime.strptime(tstr, "%b %d %Y %H:%M")
         print(dt.isoformat())
+
+        # parse title
+        title = article.find("li", class_="title").find("a").string.strip()
+        print(title)
+
+        # parse category
+        category = soup.find("ul", class_="refer").find_all("li")[1].find("a").string.strip()
+        print(category)
+
+        # parse content
+        content = soup.find("div", class_="article-content-inner")
+        print(content)
 
 
 if __name__ == '__main__':
